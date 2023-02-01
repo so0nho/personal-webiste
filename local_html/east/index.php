5f40834c22794600c3c6c6255db9fbdabb0a6407
so0nho <104328920+so0nho@users.noreply.github.com>
2023-02-01 11:14:26 -0700

    Add files via upload

---
+++ local_html/east/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Soon Ho's Personal Website</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <nav>
+        <a href="./index.php" class="nav-home">Home</a>
+        <a href="../west/index.php">West Index Page</a>
+        <a href="./one.php">Page one</a>
+        <a href="./two.php">Page two</a>
+    </nav>
+    <h1>Welcome to Soon Ho's Personal Website</h1>
+</body>
+</html>
---
+++ local_html/west/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Soon Ho's Personal Website</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li></li>
+        </ul>
+    </nav>
+    <h1>Welcome to Soon Ho's Personal Website (West page)</h1>
+</body>
+</html>
