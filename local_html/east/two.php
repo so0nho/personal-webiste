076fce1e6f91a34731f8ab129e7762bf81287da5
so0nho <104328920+so0nho@users.noreply.github.com>
2023-02-06 10:48:42 -0700

    Update two.php

--- local_html/east/two.php
+++ local_html/east/two.php
@@ -5,8 +5,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-    <h1>Page Two</h1>
+    <header>
+        <nav>
+            <ul class="navbar">
+                <li><a href="./index.php">Home</a></li>
+                <li><a href="../west/index.php">West Index Page</a></li>
+                <li><a href="./one.php">Page one</a></li>
+                <li><a href="./two.php">Page two</a></li>
+            </ul>
+        </nav>
+    </header>
+    <h1>Page Two East</h1>
 </body>
 </html>
