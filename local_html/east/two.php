e7702da36b8cae8a394f5097c201505a881081a4
so0nho <104328920+so0nho@users.noreply.github.com>
2023-02-08 10:43:37 -0700

    Update two.php

--- local_html/east/two.php
+++ local_html/east/two.php
@@ -4,20 +4,31 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>East Two</title>
+    <link rel="stylesheet" type="text/css" href="./style.css">
 </head>
-<body>
+<body class="body-east">
     <header>
         <nav>
             <ul class="navbar">
                 <li><a href="./index.php">Home</a></li>
-                <li><a href="../west/index.php">West Index Page</a></li>
-                <li><a href="./one.php">Page one</a></li>
-                <li><a href="./two.php">Page two</a></li>
+                <li><a href="../west/index.php">West</a></li>
+                <li><a href="./one.php">East One</a></li>
+                <li><a href="./two.php">East Two</a></li>
             </ul>
         </nav>
     </header>
-    <h1>Page Two East</h1>
+    <main class="main-east">
+        <h1>East Two</h1>
+        <img src="../images/italy.jpg" alt="Picture of Italy" class="img-east">
+        <blockquote cite="https://en.wikipedia.org/wiki/Lorem_ipsum">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse 
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+            non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </blockquote>
+    </main>
 </body>
 </html>
