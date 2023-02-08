629c024724bb79fb6ec9139fac92a83dbe8f2278
so0nho <104328920+so0nho@users.noreply.github.com>
2023-02-08 10:43:51 -0700

    Update index.php

--- local_html/west/index.php
+++ local_html/west/index.php
@@ -5,15 +5,35 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Soon Ho's Personal Website</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="./style.css">
 </head>
 <body>
-    <nav>
+    <header>
+        <nav>
+            <ul class="navbar">
+                <li><a href="./index.php">Home</a></li>
+                <li><a href="../east/index.php">East</a></li>
+                <li><a href="./one.php">West One</a></li>
+                <li><a href="./two.php">West Two</a></li>
+            </ul>
+        </nav>
+    </header>
+    <img src="../images/nyan-cat.gif" alt="Nyan cat gif" class="cat-gif">
+    <h1>Welcome to the West Side of My Personal Website</h1>
+    <main>
+        <p>Hello! My name is Soon Ho Kwon, I am a senior majoring in Biology.</p>
+        <p>My hobbies are:</p>
         <ul>
-            <li>Home</li>
-            <li></li>
+            <li>Hiking</li>
+            <li>Playing games</li>
+            <li>Playing sports</li>
         </ul>
-    </nav>
-    <h1>Welcome to Soon Ho's Personal Website (West page)</h1>
+        <p>I am taking this class because I wanted to learn how to make websites and I
+            think that it will be a good skill to have.
+        </p>
+    </main>
+    <footer>
+        <p>Created for CS312 Personal Website Lab</p>
+    </footer>
 </body>
 </html>
