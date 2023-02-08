22e3c6af6fea70cb6d084cd9b3cabfa9a18164ea
so0nho <104328920+so0nho@users.noreply.github.com>
2023-02-08 10:42:59 -0700

    Update index.php

--- local_html/east/index.php
+++ local_html/east/index.php
@@ -5,20 +5,35 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Soon Ho's Personal Website</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="./style.css">
 </head>
 <body>
     <header>
         <nav>
             <ul class="navbar">
                 <li><a href="./index.php">Home</a></li>
-                <li><a href="../west/index.php">West Index Page</a></li>
-                <li><a href="./one.php">Page one</a></li>
-                <li><a href="./two.php">Page two</a></li>
+                <li><a href="../west/index.php">West</a></li>
+                <li><a href="./one.php">East One</a></li>
+                <li><a href="./two.php">East Two</a></li>
             </ul>
         </nav>
     </header>
-    <img src="../nyan-cat.gif" alt="Nyan cat gif" class="cat-gif">
-    <h1>Welcome to my Personal Website</h1>
+    <img src="../images/nyan-cat.gif" alt="Nyan cat gif" class="cat-gif">
+    <h1>Welcome to the East Side of My Personal Website</h1>
+    <main>
+        <p>Hello! My name is Soon Ho Kwon, I am a senior majoring in Biology.</p>
+        <p>My hobbies are:</p>
+        <ul>
+            <li>Hiking</li>
+            <li>Playing games</li>
+            <li>Playing sports</li>
+        </ul>
+        <p>I am taking this class because I wanted to learn how to make websites and I
+            think that it will be a good skill to have.
+        </p>
+    </main>
+    <footer>
+        <p>Created for CS312 Personal Website Lab</p>
+    </footer>
 </body>
 </html>
