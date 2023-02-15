f9ee1e4d6eb3483a7c7b596658f39ac888bfab81
so0nho <104328920+so0nho@users.noreply.github.com>
2023-02-15 10:43:43 -0700

    Update two.php

---
+++ local_html/west/two.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>West Two</title>
+    <link rel="stylesheet" type="text/css" href="./style.css">
+</head>
+<body class="body-east">
+    <header>
+        <nav>
+            <ul class="navbar">
+                <li><a href="../index.html">Home</a></li>
+                <li><a href="./index.php">West Home</a></li>
+                <li><a href="../east/index.php">East</a></li>
+                <li><a href="./one.php">West One</a></li>
+                <li><a href="./two.php">West Two</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main class="main-west">
+        <h1>West Two</h1>
+        <img src="../images/italy.jpg" alt="Picture of Italy" class="img-west">
+        <blockquote cite="https://en.wikipedia.org/wiki/Lorem_ipsum">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse 
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+            non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </blockquote>
+    </main>
+</body>
+</html>
