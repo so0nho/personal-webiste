e69fba25497b0838f91c2a869c44bcf4ad1d8a93
so0nho <104328920+so0nho@users.noreply.github.com>
2023-02-15 10:42:57 -0700

    Update index.php

--- local_html/west/index.php
+++ local_html/west/index.php
@@ -7,39 +7,40 @@
     <title>Soon Ho's Personal Website</title>
     <link rel="stylesheet" type="text/css" href="./style.css">
 </head>
-
 <body>
     <header>
         <nav>
             <ul class="navbar">
                 <li><a href="../index.html">Home</a></li>
-                <li><a href="../east/index.html">East</a></li>
-                <li><a href="./one.html">West One</a></li>
-                <li><a href="./two.html">West Two</a></li>
+                <li><a href="./index.php">West Home</a></li>
+                <li><a href="../east/index.php">East</a></li>
+                <li><a href="./one.php">West One</a></li>
+                <li><a href="./two.php">West Two</a></li>
             </ul>
         </nav>
     </header>
-
     <img src="../images/nyan-cat.gif" alt="Nyan cat gif" class="cat-gif">
     
-    <h1>Welcome to the West Side of My Personal Website</h1>
-    
-    <h2 class="about-banner">About Me</h2>
-    <section class="about">
-        <p>My name is Soon Ho Kwon, I am a senior majoring in Biology. <br>I am taking this class because 
-            I wanted to learn how to make websites and I think that it will be a good skill to have.
-        </p>
-    </section>
-    
-    <h2 class="hobbies-banner">My Hobbies</h2>
-    <section class="hobbies">
-        <p>My hobbies are:</p>
-        <ul>
-            <li>Hiking</li>
-            <li>Playing games</li>
-            <li>Playing sports</li>
-        </ul>
-    </section>
+    <div class="home-left">
+        <h1>Welcome to the West Side of My Personal Website</h1>
+        
+        <h2 class="banner">About Me</h2>
+        <section class="about">
+            <p>My name is Soon Ho Kwon, I am a senior majoring in Biology. <br>I am taking this class because 
+                I wanted to learn how to make websites and I think that it will be a good skill to have.
+            </p>
+        </section>
+        
+        <h2 class="banner">My Hobbies</h2>
+        <section class="hobbies">
+            <p>My hobbies are:</p>
+            <ul>
+                <li>Hiking</li>
+                <li>Playing games</li>
+                <li>Playing sports</li>
+            </ul>
+        </section>
+    </div>
 
     <footer>
         <p>Created for CS312 Personal Website Lab</p>
