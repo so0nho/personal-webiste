503917c0ec3ac6bdebc12402f5d7938acfff5652
so0nho <104328920+so0nho@users.noreply.github.com>
2023-02-15 10:43:30 -0700

    Update one.php

---
+++ local_html/west/one.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>West One</title>
+    <link rel="stylesheet" type="text/css" href="./style.css">
+</head>
+<body class="body-east">
+    <header>
+        <nav>
+            <ul class="navbar">
+                <li><a href="../index.html">Home</a></li>
+                <li><a href="./index.php">West Home</a></li>
+                <li><a href="../east/index.php">East</a></li>
+                <li><a href="./one.php">West One</a></li>
+                <li><a href="./two.php">West Two</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main class="main-west">
+        <h1>West One</h1>
+        <img src="../images/csu.jpeg" alt="Picture of CSU" class="img-west">
+        <blockquote cite="https://en.wikipedia.org/wiki/East">
+        <p>East is the direction toward which the Earth rotates about its axis, and 
+        therefore the general direction from which the Sun appears to rise. The 
+        practice of praying towards the East is older than Christianity, but has 
+        been adopted by this religion as the Orient was thought of as containing 
+        mankind's original home. Hence, some Christian churches have been traditionally 
+        oriented towards the east. This tradition of having the altar on the 
+        liturgical east is a part of the church orientation concept liturgical east and west.</p>
+       
+        <p>The Orient is the East, traditionally comprising anything that belongs to the 
+        Eastern world, in relation to Europe. In English, it is largely a metonym for, 
+        and referring to the same area as, the continent of Asia, divided into the 
+        Far East, Middle East, and Near East. Despite this Eurocentric origin, these 
+        regions are still located to the east of the Geographical centre of Earth.</p>
+        
+        <p>Within an individual city, the east end is typically poorer because the 
+        prevailing winds blow from the west.</p>
+        </blockquote>
+    </main>
+</body>
+</html>
