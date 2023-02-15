b87ec82e72af837295e96eb42352c40fe2e5ac32
so0nho <104328920+so0nho@users.noreply.github.com>
2023-02-15 08:08:32 -0700

    Update index.php

--- local_html/east/index.php
+++ local_html/east/index.php
@@ -7,31 +7,40 @@
     <title>Soon Ho's Personal Website</title>
     <link rel="stylesheet" type="text/css" href="./style.css">
 </head>
+
 <body>
     <header>
         <nav>
             <ul class="navbar">
-                <li><a href="./index.php">Home</a></li>
-                <li><a href="../west/index.php">West</a></li>
-                <li><a href="./one.php">East One</a></li>
-                <li><a href="./two.php">East Two</a></li>
+                <li><a href="../index.html">Home</a></li>
+                <li><a href="../west/index.html">West</a></li>
+                <li><a href="./one.html">East One</a></li>
+                <li><a href="./two.html">East Two</a></li>
             </ul>
         </nav>
     </header>
+
     <img src="../images/nyan-cat.gif" alt="Nyan cat gif" class="cat-gif">
+
     <h1>Welcome to the East Side of My Personal Website</h1>
-    <main>
-        <p>Hello! My name is Soon Ho Kwon, I am a senior majoring in Biology.</p>
+
+    <h2 class="about-banner">About Me</h2>
+    <section class="about">
+        <p>My name is Soon Ho Kwon, I am a senior majoring in Biology. <br>I am taking this class because 
+            I wanted to learn how to make websites and I think that it will be a good skill to have.
+        </p>
+    </section>
+    
+    <h2 class="hobbies-banner">My Hobbies</h2>
+    <section class="hobbies">
         <p>My hobbies are:</p>
         <ul>
             <li>Hiking</li>
             <li>Playing games</li>
             <li>Playing sports</li>
         </ul>
-        <p>I am taking this class because I wanted to learn how to make websites and I
-            think that it will be a good skill to have.
-        </p>
-    </main>
+    </section>
+
     <footer>
         <p>Created for CS312 Personal Website Lab</p>
     </footer>
